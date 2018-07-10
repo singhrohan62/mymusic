8c539af2832637aef1fb35b74ce2d119108b2872
Rohan Singh <rohaniiitv@gmail.com>
2018-07-10 15:35:48 +0530

    added template for each album and filled empty song table

--- music/views.py
+++ music/views.py
@@ -1,13 +1,16 @@
-from django.http import HttpResponse
+
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
-	albums = Album.objects.all()
-	html = ' '
-	for album in albums:
-		url = '/music/' + str(album.id) 
-		html += '<a href=" '+ url + '">' + album.album_title + '</a><br>'
-	return HttpResponse(html)
+	all_albums = Album.objects.all()
+	context = {'all_albums' : all_albums}
+	return render(request,'music/index.html',context)
 
 def details(request, album_id):
-	return HttpResponse('<h2>Details for Album id : ' + str(album_id) + '</h2>')
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'music/detail.html', {'album' : album})
