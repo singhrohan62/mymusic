f1877d1edf4ca6a9f0874a372b8c0e1b795cbcdf
Rohan Singh <rohaniiitv@gmail.com>
2018-07-10 16:44:16 +0530

    designed detail view

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Album
 
 def index(request):
@@ -9,8 +9,5 @@ def index(request):
 	return render(request,'music/index.html',context)
 
 def details(request, album_id):
-	try:
-		album = Album.objects.get(pk=album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk = album_id)
 	return render(request, 'music/detail.html', {'album' : album})
