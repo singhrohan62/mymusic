8260ef9c00b80c2ef39979b58066fbd2f37d0e3a
Rohan Singh <rohaniiitv@gmail.com>
2018-07-15 00:03:05 +0530

    fixed redirecting to detail view from form-page

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import reverse
+from django.urls import reverse 
 
 class Album (models.Model):
 	artist = models.CharField(max_length=250)
@@ -8,7 +8,7 @@ class Album (models.Model):
 	album_logo = models.CharField(max_length=100)
 
 	def get_absolute_url(self):
-		return reverse('music:detail',kwargs={'pk':self.pk})
+		return reverse('music:details',kwargs={'pk':self.pk})
 
 
 	def __str__(self):
